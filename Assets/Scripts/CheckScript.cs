f51c352ba4a2914b96304aa1fda1f2a155572e02
鈴木　亮平 <ryousanngataryousann@gmail.com>
2016-06-25 16:52:43 +0900

    壁に当たったらテキスト表示

--- Assets/Scripts/CheckScript.cs
+++ Assets/Scripts/CheckScript.cs
@@ -1,25 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class CheckScript : MonoBehaviour {
 
 	public static int flag = 0;
+	public Text WallCheck;
+
 	// Use this for initialization
 	void Start () {
-	
+		WallCheck.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	void OnCollisionEnter(Collision other) {
 
-		if (other.gameObject.CompareTag("Check")) {
+		if (other.gameObject.CompareTag ("Check")) {
 			Debug.Log ("test");
 			flag++;
-			Destroy(other.gameObject);
+			Destroy (other.gameObject);
 		}
-}
+	}
+
+	void OnCollisionStay(Collision other) {
+		if (other.gameObject.CompareTag("Walls")) {
+			WallCheck.text = "これ以上進めません！";
+
+		}
+	}
+	void OnCollisionExit(Collision other){
+		WallCheck.text = "";
+	}
 }
