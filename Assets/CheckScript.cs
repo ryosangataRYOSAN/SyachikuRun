3da0e6eb16d0fa34a2bc49f9416b6c1a54793c99
鈴木　亮平 <ryousanngataryousann@gmail.com>
2016-06-25 11:08:42 +0900

    no message

---
+++ Assets/CheckScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckScript : MonoBehaviour {
+
+	public static int flag = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other) {
+
+		if (other.gameObject.CompareTag("Check")) {
+			Debug.Log ("test");
+			flag++;
+			Destroy(other.gameObject);
+		}
+}
+}
