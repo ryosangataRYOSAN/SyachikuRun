3a8fa9b6bc7f5aa1d6cb6b21c96fcc44511d37af
鈴木　亮平 <ryousanngataryousann@gmail.com>
2016-06-27 23:10:27 +0900

    何したっけこれ

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			this.transform.Rotate (15, 15, 15);
+		}
+	}
+}
