ca2fa11e806726abf7376c871f5b461417ec9e88
鈴木　亮平 <ryousanngataryousann@gmail.com>
2016-06-29 17:55:31 +0900

    簡単な車の実装とスタート画面の変更

---
+++ Assets/Scripts/CarMakeScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMakeScript : MonoBehaviour {
+
+	public GameObject car;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Generate", 0, 5);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Generate(){
+		Instantiate (car, transform.position, transform.rotation);
+		Invoke("DestroyCar", 5.0f);
+}
+
+	void DestroyCar(){
+		Destroy (this.gameObject);
+	}
+}
